2c5001c430423ac833c9495ca383ee0b91da1588
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-05-03 19:55:57 +0700

    first commit

---
+++ Q3/Program.cs
@@ -0,0 +1,101 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int letters = int.Parse(Console.ReadLine());
+
+        int digits = int.Parse(Console.ReadLine());
+
+        string lastCode = new string('A', letters) + new string('0', digits);
+
+
+        int maximumProduct = (letters * 26) * (digits * 10);
+        int count = 0;
+        
+        string product = Console.ReadLine();
+        IDictionary<string, string> productID = new Dictionary<string, string>();
+        productID.Add(lastCode,product);
+        
+        while (true)
+        {
+            product = Console.ReadLine();
+
+            if (product.ToLower() == "stop")
+            {
+                break;
+            }
+
+            if (count >= maximumProduct)
+            {
+                count = 0;
+                lastCode = new string('A', letters) + new string('0', digits);
+                productID.Add(lastCode,product);
+                continue;
+            }
+
+            string code = IncrementCode(lastCode, letters, digits);
+            productID.Add(code,product);
+            lastCode = code;
+        }
+
+        string searchCode = Console.ReadLine();
+
+        bool found = false;
+        foreach (KeyValuePair<string, string> kvp in productID)
+        {
+            if (searchCode == kvp.Key)
+            {
+                Console.WriteLine(kvp.Value);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Not found!");
+        }
+    }
+
+    static string IncrementCode(string code, int letters, int digits)
+    {
+        char[] codeArray = code.ToCharArray();
+        int lastIndex = letters + digits - 1;
+
+        for (int i = lastIndex; i >= 0; i--)
+        {
+            char c = codeArray[i];
+
+            if (char.IsDigit(c))
+            {
+                if (c == '9')
+                {
+                    codeArray[i] = '0';
+
+                    if (i == 0)
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    codeArray[i] = (char)(c + 1);
+                    break;
+                }
+            }
+            else if (char.IsLetter(c))
+            {
+                if (c == 'Z')
+                {
+                    codeArray[i] = 'A';
+                }
+                else
+                {
+                    codeArray[i] = (char)(c + 1);
+                    break;
+                }
+            }
+        }
+        return new string(codeArray);
+    }
+}
---
+++ Q4/Program.cs
@@ -0,0 +1,113 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int rows = 1;
+        int colums = 1;
+        //something such
+        Stack<int> undo = new Stack<int>();
+        while(true)
+        {
+            int action = int.Parse(Console.ReadLine());
+            if(action == 11)
+            {
+                break;
+            }
+            bool isMove = false;
+            
+            //aditional of 9 & 10
+            if(action == 10)
+            {
+
+            }
+
+            if(!isMove)
+            {
+                continue;
+            }
+            Move(action, ref rows, ref colums);
+        }
+
+        Console.WriteLine(Convert.ToChar(rows-1+'A')+" "+colums);
+    }
+    static bool CheckMove(int action, ref int rows, ref int colums)
+    {
+        if(action == 1)
+        {
+            if(rows+1 <= 8)
+            {
+                return true;
+            }
+            else if(rows + 1 <= 8 && colums - 1 > 0)
+            {
+
+            }
+            else if(colums - 1 > 0)
+            {
+                return true;
+            }
+            else if(rows - 1 > 0 && colums - 1 > 0)
+            {
+                return true;
+            }
+            else if(rows - 1 > 0)
+            {
+                return true;
+            }
+            else if(rows - 1 > 0 && colums + 1 <= 8)
+            {
+                return true;
+            }
+            else if(colums + 1 <= 8)
+            {
+                return true;
+            }
+            else if(rows + 1 <= 8 && colums + 1 <= 8)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    static void Move(int action, ref int rows, ref int colums)
+    {
+        if(action == 1)
+        {
+            if(rows+1 <= 8)
+            {
+                rows++;
+            }
+            else if(rows + 1 <= 8 && colums - 1 > 0)
+            {
+                rows++;
+                colums--;
+            }
+            else if(colums - 1 > 0)
+            {
+                colums--;
+            }
+            else if(rows - 1 > 0 && colums - 1 > 0)
+            {
+                rows--;
+                colums--;
+            }
+            else if(rows - 1 > 0)
+            {
+                rows--;
+            }
+            else if(rows - 1 > 0 && colums + 1 <= 8)
+            {
+                colums++;
+            }
+            else if(colums + 1 <= 8)
+            {
+                colums++;
+            }
+            else if(rows + 1 <= 8 && colums + 1 <= 8)
+            {
+                rows++;
+                colums++;
+            }
+        }
+    }
+}
