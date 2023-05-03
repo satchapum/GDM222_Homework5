2131204ee205350d797d4eabebaaba7d8053fed2
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-05-03 22:09:02 +0700

    finish

--- Q4/Program.cs
+++ Q4/Program.cs
@@ -4,109 +4,171 @@
     {
         int rows = 1;
         int colums = 1;
-        //something such
-        Stack<int> undo = new Stack<int>();
+        Stack<Tuple<int, int>> undoStack = new Stack<Tuple<int, int>>();
+        Stack<Tuple<int, int>> redoStack = new Stack<Tuple<int, int>>();
+
         while(true)
         {
-            int action = int.Parse(Console.ReadLine());
-            if(action == 11)
+            int move = int.Parse(Console.ReadLine());
+            int x;
+            bool canMove = false;
+
+            if(move==11)
             {
                 break;
             }
-            bool isMove = false;
-            
-            //aditional of 9 & 10
-            if(action == 10)
-            {
-
-            }
-
-            if(!isMove)
-            {
+            else if(move==9 && undoStack.Count>0){
+                Tuple<int,int> temp = new Tuple<int,int>(rows,colums);
+                rows=undoStack.Peek().Item1; 
+                colums=undoStack.Peek().Item2;  
+                redoStack.Push(temp); undoStack.Pop();
                 continue;
             }
-            Move(action, ref rows, ref colums);
+            else if(move==10 && redoStack.Count>0){
+                Tuple<int,int> temp = new Tuple<int,int>(rows,colums);
+                rows=redoStack.Peek().Item1; 
+                colums=redoStack.Peek().Item2;
+                undoStack.Push(temp); redoStack.Pop();
+                continue;
+            }
+            else if(move!=9&&move!=10){ x = int.Parse(Console.ReadLine());
+                canMove = CheckMove(move,rows,colums,x);
+
+                if(!canMove){continue;}
+                Tuple<int,int> temp = new Tuple<int,int>(rows,colums);
+                undoStack.Push(temp);
+                Move(move,ref rows,ref colums,x); 
+                while(redoStack.Count>0){redoStack.Pop();}
+            }
         }
 
-        Console.WriteLine(Convert.ToChar(rows-1+'A')+" "+colums);
+        Console.WriteLine(Convert.ToChar(colums-1+'A')+" "+rows);
     }
-    static bool CheckMove(int action, ref int rows, ref int colums)
+    static bool CheckMove(int action, int rows, int colums,int x)
     {
         if(action == 1)
         {
-            if(rows+1 <= 8)
+            if(rows + x <= 8)
             {
                 return true;
             }
-            else if(rows + 1 <= 8 && colums - 1 > 0)
-            {
-
-            }
-            else if(colums - 1 > 0)
-            {
-                return true;
-            }
-            else if(rows - 1 > 0 && colums - 1 > 0)
+        }
+        else if(action == 2)
+        {
+            if(rows + x <= 8 && colums - 1 > 0)
             {
                 return true;
             }
-            else if(rows - 1 > 0)
+        }
+        else if(action == 3)
+        {
+            if(colums - x > 0)
             {
                 return true;
             }
-            else if(rows - 1 > 0 && colums + 1 <= 8)
+        }
+        else if(action == 4)
+        {
+            if(rows - x > 0 && colums - x > 0)
             {
                 return true;
             }
-            else if(colums + 1 <= 8)
+        }
+        else if(action == 5)
+        {
+            if(rows - x > 0)
             {
                 return true;
             }
-            else if(rows + 1 <= 8 && colums + 1 <= 8)
+        }
+        else if(action == 6)
+        {
+            if(rows - x > 0 && colums + x <= 8)
             {
                 return true;
             }
+        }
+        else if(action == 7)
+        {
+            if(colums + x <= 8)
+            {
+                return true;
+            }
+        }
+        else if(action == 8)
+        {
+            if(rows + x <= 8 && colums + x <= 8)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
         }
         return false;
     }
-    static void Move(int action, ref int rows, ref int colums)
+    static void Move(int action, ref int rows, ref int colums, int x)
     {
         if(action == 1)
         {
-            if(rows+1 <= 8)
+            if(rows + x <= 8)
             {
-                rows++;
+                rows +=x ;
             }
-            else if(rows + 1 <= 8 && colums - 1 > 0)
+        }
+        else if(action == 2)
+        {
+            if(rows + x <= 8 && colums - 1 > 0)
             {
-                rows++;
-                colums--;
+                rows += x;
+                colums -= x;
             }
-            else if(colums - 1 > 0)
+        }
+        else if(action == 3)
+        {
+            if(colums - x > 0)
             {
-                colums--;
+                colums -= x;
             }
-            else if(rows - 1 > 0 && colums - 1 > 0)
+        }
+        else if(action == 4)
+        {
+            if(rows - x > 0 && colums - x > 0)
             {
-                rows--;
-                colums--;
+                rows -= x;
+                colums -= x;
+                
             }
-            else if(rows - 1 > 0)
+        }
+        else if(action == 5)
+        {
+            if(rows - x > 0)
             {
-                rows--;
+                rows -= x;
             }
-            else if(rows - 1 > 0 && colums + 1 <= 8)
+        }
+        else if(action == 6)
+        {
+            if(rows - x > 0 && colums + x <= 8)
             {
-                colums++;
+                rows -= x;
+                colums += x;
             }
-            else if(colums + 1 <= 8)
+        }
+        else if(action == 7)
+        {
+            if(colums + x <= 8)
             {
-                colums++;
+                colums += x;
             }
-            else if(rows + 1 <= 8 && colums + 1 <= 8)
+        }
+        else if(action == 8)
+        {
+            if(rows + x <= 8 && colums + x <= 8)
             {
-                rows++;
-                colums++;
+                rows += x;
+                colums += x;
             }
         }
     }
